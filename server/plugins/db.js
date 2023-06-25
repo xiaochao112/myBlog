32f6c8367a01158fd376957728345effd50f3525
myGre <1745083253@qq.com>
2023-06-25 16:50:46 +0800

    添加远程数据库

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -2,22 +2,22 @@ module.exports = (app) => {
   const mongoose = require("mongoose");
   const db_user = 'hwc'; // 数据库用户名
   const db_pwd = '123456'; // 数据库密码
-  const db_name = 'mydb'; // 数据库名字
-  const db_host = '175.178.191.198'; // 数据库地址
+  const db_name = 'test'; // 数据库名字
+  const db_host = '139.159.137.145'; // 数据库地址
 
-  // mongoose.connect(`mongodb://${db_user}:${db_pwd}@${db_host}:27017/${db_name}?authSource=admin`, {
-  //   useNewUrlParser: true,
-  //   useFindAndModify: true,
-  //   useUnifiedTopology: true,
-  //   useCreateIndex: true,
-  //   useFindAndModify: true
-  // });
-  mongoose.connect(`mongodb://localhost:27017/${db_name}`, {
+  mongoose.connect(`mongodb://${db_user}:${db_pwd}@${db_host}:27017/${db_name}?authSource=test`, {
     useNewUrlParser: true,
     useFindAndModify: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: true
   });
+  // mongoose.connect(`mongodb://localhost:27017/${db_name}`, {
+  //   useNewUrlParser: true,
+  //   useFindAndModify: true,
+  //   useUnifiedTopology: true,
+  //   useCreateIndex: true,
+  //   useFindAndModify: true
+  // });
   require("require-all")(__dirname + "/../models");
 };
