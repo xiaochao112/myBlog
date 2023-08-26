86722eb8c1e072d3d19664032465a00beffc0cef
myGre <1745083253@qq.com>
2023-08-26 16:59:26 +0800

    更改数据库配置文件

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
   const mongoose = require("mongoose");
-  const db_user = 'hwc'; // 数据库用户名
-  const db_pwd = '123456'; // 数据库密码
-  const db_name = 'test'; // 数据库名字
-  const db_host = '139.159.137.145'; // 数据库地址
+  const db_user = ''; // 数据库用户名
+  const db_pwd = ''; // 数据库密码
+  const db_name = ''; // 数据库名字
+  const db_host = ''; // 数据库地址
 
   mongoose.connect(`mongodb://${db_user}:${db_pwd}@${db_host}:27017/${db_name}?authSource=test`, {
     useNewUrlParser: true,
