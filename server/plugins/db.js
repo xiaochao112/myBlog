fff73d5d3b1522504165c2448a006be3040e3b72
myGre <1745083253@qq.com>
2023-09-02 10:39:50 +0800

    重新添加db文件

---
+++ server/plugins/db.js
@@ -0,0 +1,24 @@
+module.exports = (app) => {
+  const mongoose = require("mongoose");
+  const db_user = ''; // 数据库用户名
+  const db_pwd = ''; // 数据库密码
+  const db_auth = ''; // 验证的数据库
+  const db_name = ''; // 数据库名字
+  const db_host = ''; // 数据库地址
+
+  mongoose.connect(`mongodb://${db_user}:${db_pwd}@${db_host}:27017/${db_name}?authSource=${db_auth}`, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  });
+  // mongoose.connect(`mongodb://localhost:27017/${db_name}`, {
+  //   useNewUrlParser: true,
+  //   useFindAndModify: true,
+  //   useUnifiedTopology: true,
+  //   useCreateIndex: true,
+  //   useFindAndModify: true
+  // });
+  require("require-all")(__dirname + "/../models");
+};
